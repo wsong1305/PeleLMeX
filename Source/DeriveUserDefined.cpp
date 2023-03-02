7427b56abf45978b72f264b5634d1d0ee71b4c66
Lucas Esclapez <lucas.esclapez@nrel.gov>
2023-03-01 23:38:29 -0800

    Add documentation on the use of the user-defined derived variable.

--- Source/DeriveUserDefined.cpp
+++ Source/DeriveUserDefined.cpp
@@ -19,9 +19,27 @@ Vector<std::string> pelelm_setuserderives()
 //
 // User-defined derived definition
 //
-void pelelm_deruserdef (PeleLM* /*a_pelelm*/, const Box& /*bx*/, FArrayBox& /*derfab*/, int /*dcomp*/, int /*ncomp*/,
-                        const FArrayBox& /*statefab*/, const FArrayBox& /*reactfab*/, const FArrayBox& /*pressfab*/,
+void pelelm_deruserdef (PeleLM* /*a_pelelm*/, const Box& bx, FArrayBox& derfab, int dcomp, int ncomp,
+                        const FArrayBox& statefab, const FArrayBox& /*reactfab*/, const FArrayBox& /*pressfab*/,
                         const Geometry& /*geom*/, Real /*time*/, const Vector<BCRec>& /*bcrec*/, int /*level*/)
 {
+    // Checks
+    AMREX_ASSERT(derfab.box().contains(bx));
+    AMREX_ASSERT(statefab.box().contains(bx));
+    AMREX_ASSERT(derfab.nComp() >= dcomp + ncomp);
+
+    // Array4s
+    auto const in_dat = statefab.array();
+    auto       der = derfab.array(dcomp);
+
+    /* Implement your own function in the lambda below
+       Example of derived are available in Source/PeleLMDeriveFunc.cpp
+    amrex::ParallelFor(bx,
+    [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+    {
+        der(i,j,k) = in_dat(i,j,k,DENSITY) * in_dat(i,j,k,VELX);
+    });
+    */
+
     Abort("Using derUserDefine derived requires providing a definition in local DeriveUserDefined.cpp");
 }
