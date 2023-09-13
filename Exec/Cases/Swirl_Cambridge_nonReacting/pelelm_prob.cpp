05728ab04875d24b2bc7a5f3b20235c0f46891a0
Bruno Soriano <bsouzas@sandia.gov>
2023-09-13 16:58:22 -0700

    Non-reacting version of the Cambridge burner

---
+++ Exec/Cases/Swirl_Cambridge_nonReacting/pelelm_prob.cpp
@@ -0,0 +1,19 @@
+#include <PeleLM.H>
+#include <AMReX_ParmParse.H>
+
+void PeleLM::readProbParm()
+{
+  amrex::ParmParse pp("prob");
+
+   pp.query("P_mean"       , PeleLM::prob_parm->P_mean);
+   pp.query("Zst"          , PeleLM::prob_parm->Zst);
+   pp.query("T_in"         , PeleLM::prob_parm->T_in);
+   pp.query("U_b"          , PeleLM::prob_parm->U_b);
+   pp.query("standoff"     , PeleLM::prob_parm->standoff);
+
+
+   PeleLM::prob_parm->bathID = N2_ID;  
+   PeleLM::prob_parm->oxidID = O2_ID; 
+
+   PeleLM::pmf_data.initialize();
+}
